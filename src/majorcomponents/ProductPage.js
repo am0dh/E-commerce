fefd4e8fb9c8f533e7ffaee48c005d496b54eec1
Amodh <amodhmandloi2@gmail.com>
2020-12-04 17:28:43 +0530

    initial

--- src/majorcomponents/ProductPage.js
+++ src/majorcomponents/ProductPage.js
@@ -2,7 +2,9 @@ import React, { useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import Product from "./singleproduct/Product";
 import { connect } from "react-redux";
+import { fetchProductTypes,fetchProductsByType } from "../actions/productTypesAction";
 
+function ProductPage() {
 
     const productTypes=useSelector(state=> state.productReducer)
     const dispatch = useDispatch()
